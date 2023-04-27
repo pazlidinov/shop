804127df161c3a1e863d562ceaf2460552f2c47a
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-04-27 13:31:46 +0500

    add count function

---
+++ config/staticfiles/js/detail_page.js
@@ -0,0 +1,15 @@
+// let count = document.getElementById(id = 'count')
+// num = Number(count.value)
+// console.log(typeof (count.value))
+function minus() {
+    let count = document.getElementById(id = 'count')
+    num = Number(count.value)
+    if (num > 1) {
+        count.value = (num - 1).toString()
+    }
+}
+function plus() {
+    let count = document.getElementById(id = 'count')
+    num = Number(count.value)
+    count.value = (num + 1).toString()
+}
