44d81bce1ad1cc6013c26b72eeb7550c18fbd10c
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-04-27 10:05:43 +0500

    repair detail page

--- config/main_app/models.py
+++ config/main_app/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-
-
 class Size(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
