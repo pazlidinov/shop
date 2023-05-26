6348b2a73694a044bfbd0156dfc6a6961908d870
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-05-26 16:23:30 +0500

    add liked and cart count

--- config/main_app/context_cat.py
+++ config/main_app/context_cat.py
@@ -1,9 +1,13 @@
 from .models import Category, Product
-
+from cart.models import LikedCart, CardProduct
 
 def main_context(request):
-    categories = Category.objects.all()   
+    categories = Category.objects.all() 
+    liked_count=LikedCart.objects.filter(id=request.session['user_liked_id'])
+    cart_count=CardProduct.objects.filter(products=request.session.get('user_cart_id'))
     data = {
-        'category': categories,        
+        'category': categories,  
+        'liked_count':liked_count,
+        'cart_count':cart_count,
     }
     return data
